bc4175cc71d38a4f6549b63f880733311d49295f
yanovskaya <yanovskaya1997@list.ru>
2018-05-26 11:06:32 +0300

    комментарии ->  markup

--- DI.playground/Contents.swift
+++ DI.playground/Contents.swift
@@ -1,24 +1,11 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-// Dependancy Injection
-
-// Вместо того, чтобы создавать зависимость внутри
-
-protocol Service1 {}
-class RealService1: Service1 {}
-
-class Client1 {
-    let service: Service1
-    
-    init() {
-        service = RealService1()
-    }
-}
-
-// Объект может получить ее снаружи.
-
+/*:
+ Dependency Injection в Swift 4.1
+ ====
+ Краткое описание dependency injection в языке Swift.
+ */
+/*:
+Вместо того, чтобы создавать зависимость внутри ...
+*/
 
 protocol Service {}
 class RealService: Service {}
@@ -26,31 +13,55 @@ class RealService: Service {}
 class Client {
     let service: Service
     
-    init(service: Service) {
+    init() {
+        service = RealService() // зависимость внутри инициализатора
+    }
+}
+
+/*:
+... объект должен получить ее снаружи.
+*/
+
+protocol ServiceDI {}
+class RealServiceDI: ServiceDI {}
+
+class ClientDI {
+    let service: ServiceDI
+    
+    init(service: ServiceDI) {
         self.service = service // зависимости внутри нет, объект получит ее снаружи при инициализации
     }
 }
 
-// Объекты сразу становятся полностью тестируемыми. Тестируемый дубль вместо реального сервиса может быть передан инициализатору, чтобы получать ожидаемые респонсы или шпионить за тем, что вызвано службой.
+/*:
+ Объекты сразу становятся полностью тестируемыми. Тестируемый дубль вместо реального сервиса может быть передан инициализатору, чтобы получать ожидаемые респонсы или шпионить за тем, что вызвано службой.
+ 
+**Есть 3 типа DI:**
+ + setter-
+ + interface- (редко используется)
+ + constructor-based
+ 
+## **Constructor = initializer-based injection**
 
-// Есть 3 типа DI:
-// setter-
-// interface- (редко)
-// constructor-based
+Зависимость передается клиенту в инициализаторе и **не изменяется в течение всего срока жизни клиента**.
+Cамое большое преимуществоо - он делает очевидным нарушение принципа единой ответственности. Если объект принимает все свои зависимости в инициализаторе, а инициализатор имеет более трех параметров, это намек на то, что объект может выполнять несколько операций, и рефакторинг необходим.
 
-// Constructor = initializer-based injection
-// Это когда зависимость передается клиенту в инициализаторе и не изменяется в течение всего срока жизни клиента.
-// Однако самым большим преимуществом такого типа может быть тот факт, что он делает очевидным нарушение принципа единой ответственности. Если объект принимает все свои зависимости в инициализаторе, а инициализатор имеет более трех параметров, это намек на то, что объект может выполнять несколько операций, а рефакторинг необходим.
+Рекомендуется использовать constructor injection для зависимости, **которая не изменяется во время жизни зависимого объекта**.
 
+## **Setter injection**
+ 
+Если зависимость (engine) может меняться в течение жизни зависимого объекта (car) или когда при инициализации это nil, лучше использовать setter injection.
 
-// Рекомендуется использовать constructor injection для зависимости, которая не изменяется во время жизни зависимого объекта
-// Когда зависимость может меняться в течение жизни зависимого объекта или когда при инициализации это nil, лучше использовать setter injection
-
-// Примеры setter injection:
-// car.setEngine(rocketEngine)
-// car.engine = rocketEngine
-
-
-// Источники:
-// https://medium.com/ios-os-x-development/dependency-injection-in-swift-a959c6eee0ab
-// https://learnappmaking.com/dependency-injection-swift/
+ Примеры setter injection:
+```
+ car.setEngine(rocketEngine)
+ ```
+```
+ car.engine = rocketEngine
+ ```
+*/
+/*:
+### Источники:
++ https://medium.com/ios-os-x-development/dependency-injection-in-swift-a959c6eee0ab
++ https://learnappmaking.com/dependency-injection-swift/
+*/
